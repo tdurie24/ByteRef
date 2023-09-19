58c24251aeb11726d7bc578ec9063069a2a57c95
Daniel T Mpofu <50142620+danielmpofu@users.noreply.github.com>
2023-09-19 15:56:01 +0200

    added delivery companies service

---
+++ orders-pick-and-pack/src/app/@core/services/distribution.companies.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {BehaviorSubject, Observable} from "rxjs";
+import {LogisticsStatus} from "../models/logistics.status";
+import {HttpClient} from "@angular/common/http";
+import {DistributionCompanyModel} from "../models/distribution.company.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DistributionCompaniesService {
+
+    private distributionCompaniesBaseUrl = environment.apiUrl + "DistributionCompanies";
+
+    private distributionSource: BehaviorSubject<DistributionCompanyModel[] | null> = new BehaviorSubject<DistributionCompanyModel[] | null>(null);
+    distributionSourceObservable = this.distributionSource.asObservable();
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    getDistributionCompanies() {
+        this.httpClient.get<DistributionCompanyModel[]>(this.distributionCompaniesBaseUrl).subscribe(
+            {
+                next: response => {
+                    this.distributionSource.next(response);
+                    console.log(response);
+                }, error: err => {
+                    console.log(err);
+                }
+            }
+        )
+    }
+}
